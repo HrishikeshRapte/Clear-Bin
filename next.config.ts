b5de76f0332aaeec5a1b95326bc8dae77e323b6a
Hrishikesh Rapte <135985402+HrishikeshRapte@users.noreply.github.com>
2025-06-12 20:17:13 +0530

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -10,11 +10,23 @@
 import { Database } from "lucide-react";
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
     Web3_Auth_CLIENT_ID: process.env.WEB3_AUTH_CLIENT_ID,
     NEXT_PUBLIC_GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+  },
+
+
+  webpack: (config, { isServer }) => {
+  if (isServer) {
+    config.externals.push('@react-native-async-storage/async-storage');
   }
+  return config;
+}
+
+
 };
+export default nextConfig;
